8b63f242b82ad790544202278e82b46c9360242a
foxxed12 <foxxed20@yandex.ru>
2025-07-09 14:27:14 +0300

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import dynamic from "next/dynamic";
-const BirthdayGiftVika = dynamic(
-  () => import("../components/BirthdayGiftVika"),
-  { ssr: false }
-);
-
-export default function Home() {
-  return <BirthdayGiftVika />;
-}
