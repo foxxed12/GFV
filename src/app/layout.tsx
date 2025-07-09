71a1febbf29e6fcad21c88ea51c885b824dc58a4
foxxed12 <foxxed20@yandex.ru>
2025-07-09 15:13:50 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,12 @@
-tsx import "@/app/globals.css";
+import "@/app/globals.css";
+
 export const metadata = { title: "Happy Birthday Vika" };
 
-export default function Root({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ru">
       <body className="font-sans antialiased">{children}</body>
