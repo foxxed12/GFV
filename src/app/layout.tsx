5a18b84faac3aec3b1be55f8ea7bd56f4350aa6e
foxxed12 <foxxed20@yandex.ru>
2025-07-09 14:21:12 +0300

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+export const metadata = { title: "Happy Birthday Vika" };
+
+export default function Root({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="ru">
+      <body className="font-sans antialiased">{children}</body>
+    </html>
+  );
+}
