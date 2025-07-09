38dc2c76108f7438cce5b00ec9edf2aa5de956d3
foxxed12 <foxxed20@yandex.ru>
2025-07-09 14:20:07 +0300

    Create input.tsx

---
+++ src/components/ui/input.tsx
@@ -0,0 +1,6 @@
+export const Input = (p: React.InputHTMLAttributes<HTMLInputElement>) => (
+  <input
+    {...p}
+    className="w-full rounded-md border px-3 py-2 outline-none focus:ring-2 focus:ring-pink-400"
+  />
+);
